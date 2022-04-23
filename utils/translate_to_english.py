86d9149bb3e33bdec082baa996433d235dd257a9
Zilu Tang <zilu.p.tang@gmail.com>
2022-04-23 01:34:16 -0400

    fix jsonl load behavior

--- utils/translate_to_english.py
+++ utils/translate_to_english.py
@@ -1,3 +1,4 @@
+import json
 import os
 import argparse
 
@@ -14,14 +15,16 @@ def translate(in_path: str, out_path: str):
 
     model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-ny")
 
-    pipe = TranslationPipeline(model, tokenizer=tokenizer, device=1, batch_size=32)
+    pipe = TranslationPipeline(model, tokenizer=tokenizer, device=-1, batch_size=32)
 
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
     kwargs = {'truncation': True, 'max_length': 5000, 'return_text': True, "clean_up_tokenization_spaces": True}
     if in_path.endswith(".txt"):
         dataset = datasets.load_dataset('text', data_files={'train': [in_path]})["train"]
-    elif in_path.endswith(".jsonl"):
-        dataset = datasets.load_dataset('json', data_files={'train': [in_path]}, field='text')["train"]
+    elif ".jsonl" in in_path:
+        dataset = datasets.load_dataset('text', data_files={'train': [in_path]})["train"]
+        dataset = dataset.map(lambda example: {"content": json.loads(example["text"])["text"]}, remove_columns=["text"])
+        dataset = dataset.rename_column("content", "text")
     with open(out_path, "w") as out_f:
         for out in tqdm(pipe(KeyDataset(dataset, "text"), **kwargs)):
             out_f.writelines(out[0]["translation_text"] + "\n")
@@ -29,7 +32,7 @@ def translate(in_path: str, out_path: str):
 
 if __name__ == "__main__":
     # translate("../data/english_news/coca-samples-text/text_mag.txt", "./test.txt")
-
+    # translate("../data/english_news/realnews/realnews.jsonl9050", "./test.txt")
     my_parser = argparse.ArgumentParser(description='')
     my_parser.add_argument('-in_file',
                            type=str,
