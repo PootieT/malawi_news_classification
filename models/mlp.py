40fe176a5f5c5c40806c0da330521d42cffa4fbf
Saurav vara prasad Chennuri <saurav@dhcp-wifi-8021x-155-41-60-43.bu.edu>
2022-05-01 19:35:36 -0400

    mlp code

--- models/mlp.py
+++ models/mlp.py
@@ -8,13 +8,13 @@ from tqdm import tqdm
 
 
 class NeuralNetwork(nn.Module):
-  def __init__(self, input_size):
+  def __init__(self, input_size, output_size):
     super(NeuralNetwork, self).__init__()
     # You are free to define any number of layers you want to here
     self.layer1 = nn.Linear(input_size, 800) # Input Layer
     self.layer2 = nn.Linear(800, 750)        # Second Layer
     self.layer3 = nn.Linear(750, 300)        # Third Layer
-    self.output = nn.Linear(300, 10)         # Output Layer
+    self.output = nn.Linear(300, output_size)# Output Layer
     self.dropout = nn.Dropout(0.2)
 
 
