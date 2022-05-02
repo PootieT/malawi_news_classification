e49643c270c6ea9f9083edc9c936a90e1190dddb
Saurav vara prasad Chennuri <saurav@nat-wireless-guest-reg-153-51.bu.edu>
2022-05-02 15:24:21 -0400

    main file

--- experiments/main.py
+++ experiments/main.py
@@ -4,16 +4,12 @@ from train_baseline_chichewa import *
 
 
 def results(df) -> None: 
-    """
-    :param df : dataframe of results per model per vectorizer
-    """
-    raise NotImplementedError
-
     
+    raise NotImplementedError
 
 if __name__ == "__main__":
     chichewa_result = train_and_evaluate(data_path="../data/train.csv")
-    chichewa_result.to_csv('../results/chichewa_eval.csv')
-    # results(chichewa_result)
+    chichewa_result.to_csv('../results/chichewa_eval_final.csv')
+
 
     
