6d602afc2bb2b7b4da855f0a9131a1a7dd6cc598
Saurav vara prasad Chennuri <saurav@nat-wireless-guest-reg-153-51.bu.edu>
2022-05-02 15:37:58 -0400

    added cross validation

--- experiments/train_baseline_chichewa.py
+++ experiments/train_baseline_chichewa.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.model_selection import KFold
 import sys
 # insert at 1, 0 is the script path (or '' in REPL)
 sys.path.insert(0, '../models')
@@ -12,21 +13,29 @@ def train_and_evaluate(data_path: str):
     X_train, X_test, y_train, y_test = train_test_split(
         train_df.Text, train_df.Label, test_size=0.3, random_state=42, stratify=train_df.Label)
     
-    test_df = pd.read_csv("../data/test.csv")
+    language = "chichewa"
 
-    metrics = []
-    combo = []
+    test_df = pd.read_csv("../data/test.csv")
+    kf = KFold(n_splits=2, random_state=None, shuffle=False)
+    
     for vec in ["tfidf", "cv", "MT5"]:
         for m in ["NB", "LR", "XGB", "MLP", "RF"]:
-            combo.append(vec + "-" + m)
-            model = BaselineClassifier(vec, m, verbose=False)
-            model.train_supervised(X_train, y_train)
-            metric = model.evaluate(X_test, y_test)
-            metric["vectorizer"], metric["classifier"] = vec, m
-            metrics.append(metric)
+            metrics = []
+            combo = []
+            for train_index, test_index in kf.split(train_df.Text):
+                print("TRAIN:", train_index, "TEST:", test_index)
+                X_train, X_test = train_df.iloc[train_index].Text, train_df.iloc[test_index].Text
+                y_train, y_test = train_df.iloc[train_index].Label, train_df.iloc[test_index].Label
+                combo.append(vec + "-" + m)
+                model = BaselineClassifier(vec, m, verbose=False)
+                model.train_supervised(X_train, y_train)
+                metric = model.evaluate(X_test, y_test)
+                metric["vectorizer"], metric["classifier"] = vec, m
+                metrics.append(metric)
 
-    eval_df = pd.DataFrame(metrics)
-    eval_df.index = combo
+            eval_df = pd.DataFrame(metrics)
+            eval_df.to_csv("../results/"+language+"_" + vec + "_" + m +".csv")
+            eval_df.index = combo
     return eval_df
 
 
