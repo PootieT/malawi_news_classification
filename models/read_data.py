9c69e61ced2c2c53e030fad0b983b54f2b47b590
Saurav vara prasad Chennuri <saurav@nat-wireless-guest-reg-153-51.bu.edu>
2022-05-02 13:17:32 -0400

    reads files

--- models/read_data.py
+++ models/read_data.py
@@ -5,25 +5,30 @@ import os
 
 PATH = "../data/custom_data/"
 
-MT5 = "mt5_embeddings"
+MT5 = "embeddings_chichewa_512_final.npy"
 CONTASTIVE = "contrastive_embeddings"
 
 
 def read_data(file):
     
-    extension = os.path.splitext(file)[1]
+    extension = os.path.splitext(file)[1][1:]
 
     if extension == "npz":
         file = np.load(file)
         data = file["arr_0"]
         labels = file["arr_1"]
-        return data, labels 
+        return data
+    
+    if extension == "npy":
+        data = np.load(PATH+file)
+        return data
+
 
     elif extension == "csv":
         file = pd.read_csv(file)
         data = file.drop(["labels"], axis=1).to_numpy()
         labels = list(file["labels"])
-        return data, labels
+        return data
     
     elif extension == "pkl":
         raise NotImplementedError
