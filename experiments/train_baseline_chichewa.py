f706200eb9bd4cf5e9357f4c082a3230e3f1f214
Saurav vara prasad Chennuri <saurav@nat-wireless-guest-reg-153-51.bu.edu>
2022-05-02 15:24:44 -0400

    modified for custom embeddings and text

--- experiments/train_baseline_chichewa.py
+++ experiments/train_baseline_chichewa.py
@@ -17,7 +17,7 @@ def train_and_evaluate(data_path: str):
     metrics = []
     combo = []
     for vec in ["tfidf", "cv", "MT5"]:
-        for m in ["NB", "LR"]:
+        for m in ["NB", "LR", "XGB", "MLP", "RF"]:
             combo.append(vec + "-" + m)
             model = BaselineClassifier(vec, m, verbose=False)
             model.train_supervised(X_train, y_train)
