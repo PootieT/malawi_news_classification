20fee70cd02cc163761adc8fdc44b714ae671f7f
Zilu Tang <zilu.p.tang@gmail.com>
2022-05-02 12:03:12 -0400

    add latest

---
+++ analysis/visualize_alignment.py
@@ -0,0 +1,47 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.decomposition import PCA
+import matplotlib.patches as mpatches
+
+
+def get_cmap(n, name='prism'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+
+def visualize_alignment(emb_ny: np.array, emb_en: np.array, train_df: pd.DataFrame, title: str):
+    emb_all = np.vstack([emb_ny, emb_en])
+    pca = PCA(n_components=2)
+    result = pca.fit_transform(emb_all)
+    # create a scatter plot of the projection
+    plt.figure(figsize=(15, 12))
+
+    # plt.scatter(result[:, 0], result[:, 1])
+
+    color_labels = train_df.Label
+    unique_color_labels = list(set(color_labels))
+    cmap = get_cmap(len(unique_color_labels))
+    color2idx = {c: i for i, c in enumerate(unique_color_labels)}
+
+    for i, clabel in enumerate(color_labels):
+        # plt.annotate(label, xy=(result[i, 0], result[i, 1]))
+        plt.plot(result[i, 0], result[i, 1], "o", c=cmap(color2idx[clabel]))
+        plt.plot(result[i+len(train_df), 0], result[i+len(train_df), 1], "x", c=cmap(color2idx[clabel]))
+
+    # plt.show()
+    legend_patches = [mpatches.Patch(color=cmap(color2idx[c]), label=c) for c in unique_color_labels]
+
+    plt.legend(handles=legend_patches)
+    plt.title(title)
+    plt.savefig(f"{title}.png")
+
+
+if __name__ == "__main__":
+    emb_chichewa = np.load("../data/mt5_embeddings/embeddings_chichewa_512_final.npy")
+    emb_english = np.load("../data/mt5_embeddings/embeddings_english_512_final.npy")
+    train_df = pd.read_csv("../data/train.csv")
+    visualize_alignment(emb_chichewa, emb_english, train_df, title="mt5 embedding visualization")
--- utils/translate_to_english.py
+++ utils/translate_to_english.py
@@ -10,16 +10,19 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers.pipelines.pt_utils import KeyDataset
 
 
-def translate(in_path: str, out_path: str, batch_size:int=32):
-    tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-ny")
-
-    model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-ny")
+def translate(in_path: str, out_path: str, batch_size:int=32, en_to_ny:bool = True):
+    model_name = "Helsinki-NLP/opus-mt-en-ny" if en_to_ny else "Helsinki-NLP/opus-mt-ny-en"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
     pipe = TranslationPipeline(model, tokenizer=tokenizer, device=0, batch_size=batch_size)
 
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
     kwargs = {'truncation': True, 'max_length': 512, 'return_text': True, "clean_up_tokenization_spaces": True}
-    if in_path.endswith(".txt"):
+    if in_path.endswith(".csv"):
+        dataset = datasets.load_dataset('csv', data_files={'train': [in_path]})["train"]
+        dataset = dataset.rename_column("Text", "text")
+    elif in_path.endswith(".txt"):
         dataset = datasets.load_dataset('text', data_files={'train': [in_path]})["train"]
     elif ".jsonl" in in_path:
         dataset = datasets.load_dataset('text', data_files={'train': [in_path]})["train"]
@@ -35,17 +38,18 @@ def translate(in_path: str, out_path: str, batch_size:int=32):
 if __name__ == "__main__":
     # translate("../data/english_news/coca-samples-text/text_mag.txt", "./test.txt")
     # translate("../data/english_news/realnews/realnews.jsonl9050", "./test.txt")
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-in_file',
-                        type=str,
-                        help='the path to translation source file (in English)')
-    parser.add_argument('-out_file',
-                        type=str,
-                        help='the path to translation target file (in Nyanja) aka output')
-    parser.add_argument('-batch_size',
-                        type=int,
-                        help='batch_size for pipeline')
-
-    args = parser.parse_args()
-
-    translate(args.in_file, args.out_file, args.batch_size)
+    translate("../data/train.csv", "../data/train_helsinki_translated.txt", batch_size=16, en_to_ny=False)
+    # parser = argparse.ArgumentParser(description='')
+    # parser.add_argument('-in_file',
+    #                     type=str,
+    #                     help='the path to translation source file (in English)')
+    # parser.add_argument('-out_file',
+    #                     type=str,
+    #                     help='the path to translation target file (in Nyanja) aka output')
+    # parser.add_argument('-batch_size',
+    #                     type=int,
+    #                     help='batch_size for pipeline')
+    #
+    # args = parser.parse_args()
+    #
+    # translate(args.in_file, args.out_file, args.batch_size)
