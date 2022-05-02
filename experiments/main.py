7697d86795a92637d5e82d126e497fdd4b4b9bc9
Saurav vara prasad Chennuri <saurav@nat-wireless-guest-reg-153-51.bu.edu>
2022-05-02 15:38:14 -0400

    main update

--- experiments/main.py
+++ experiments/main.py
@@ -4,12 +4,11 @@ from train_baseline_chichewa import *
 
 
 def results(df) -> None: 
-    
     raise NotImplementedError
 
 if __name__ == "__main__":
-    chichewa_result = train_and_evaluate(data_path="../data/train.csv")
-    chichewa_result.to_csv('../results/chichewa_eval_final.csv')
+    train_and_evaluate(data_path="../data/train.csv")
+    
 
 
     
