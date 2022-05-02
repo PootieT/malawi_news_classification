ddea1cdace0990840efb316a55c4d9f63b992d80
Saurav vara prasad Chennuri <saurav@nat-wireless-guest-reg-153-51.bu.edu>
2022-05-02 11:52:36 -0400

    modified for streamlining things

--- experiments/train_baseline_chichewa.py
+++ experiments/train_baseline_chichewa.py
@@ -1,20 +1,24 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
-from models.model_baselines import BaselineClassifier
-
+import numpy as np
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(0, '../models')
+from model_baselines import BaselineClassifier
 
 def train_and_evaluate(data_path: str):
     train_df = pd.read_csv(data_path)
 
     X_train, X_test, y_train, y_test = train_test_split(
         train_df.Text, train_df.Label, test_size=0.3, random_state=42, stratify=train_df.Label)
-
-    # test_df = pd.read_csv("../data/test.csv")
+    
+    test_df = pd.read_csv("../data/test.csv")
 
     metrics = []
+    combo = []
     for vec in ["tfidf", "cv"]:
         for m in ["NB", "LR"]:
+            combo.append(vec + "-" + m)
             model = BaselineClassifier(vec, m, verbose=False)
             model.train_supervised(X_train, y_train)
             metric = model.evaluate(X_test, y_test)
@@ -22,10 +26,9 @@ def train_and_evaluate(data_path: str):
             metrics.append(metric)
 
     eval_df = pd.DataFrame(metrics)
+    eval_df.index = combo
     return eval_df
 
 
-if __name__ == "__main__":
-    chichewa_result = train_and_evaluate(data_path="../data/train.csv")
-    english_result = train_and_evaluate(data_path="../data/train_google_translated.csv")
-    pass
+
+
