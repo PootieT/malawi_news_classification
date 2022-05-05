bc0f7a6b4ec64a7bf1b5a2f08e8a86947c5f3fa9
Zilu Tang <zilu.p.tang@gmail.com>
2022-05-04 21:57:28 -0400

    update training with embedding

--- models/network.py
+++ models/network.py
@@ -21,3 +21,19 @@ class Network(nn.Module):
         x = self.dropout(x)
         outputs = self.output(x)
         return F.softmax(outputs, 1)
+
+
+class NetworkSmall(nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super(NetworkSmall, self).__init__()
+        # You are free to define any number of layers you want to here
+        self.layer1 = nn.Linear(input_size, 2048) # Input Layer
+        self.output = nn.Linear(2048, output_size)# Output Layer
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, input):
+        x = F.relu(self.layer1(input))
+        x = self.dropout(x)
+        outputs = self.output(x)
+        return F.softmax(outputs, 1)
